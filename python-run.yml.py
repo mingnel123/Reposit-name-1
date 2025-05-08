174343996fa9c69ea739ba3fdc10cb90dedaed9b
mingnel123 <114153476+mingnel123@users.noreply.github.com>
2025-05-08 16:27:13 +0800

    Update python-run.yml.py

--- python-run.yml.py
+++ python-run.yml.py
@@ -1,32 +1,5 @@
 #question (d)
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 import numpy as np
 
-# Read the CSV file
-data = pd.read_csv('expectancy.csv')
-
-# Extract the columns from the DataFrame
-year = data['Year']
-age = data['Age']
-
-# Perform linear regression (OLS)
-slope, intercept = np.polyfit(year, age, 1)
-ols_line = slope * np.array(year) + intercept
-
-# Create the scatter plot using Seaborn
-sns.scatterplot(data=data, x='Year', y='Age', label='Age Over Time')
-
-# Plot the OLS line
-plt.plot(year, ols_line, color='red', label='OLS Line')
-
-# Set the plot labels and title
-plt.xlabel('Year')
-plt.ylabel('Age')
-plt.title('Hong Kong Life(Age Over Time)')
-
-# Display the plot
-plt.legend()
-plt.show()
-
+print("Try programiz.pro")
